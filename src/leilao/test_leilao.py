5ce171fb6b14257603c863eb654de98a38b795be
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-06-18 17:52:04 -0300

    Makes course class "Testando exceções e TDD"

--- src/leilao/test_leilao.py
+++ src/leilao/test_leilao.py
@@ -77,9 +77,6 @@ class TestLeilao(TestCase):
     def test_nao_deve_permitir_propor_lance_caso_o_usuario_seja_o_mesmo(self):
         lance_do_gui200 = Lance(self.gui, 200.0)
 
-        self.leilao.propoe(self.lance_do_gui)
-        self.leilao.propoe(lance_do_gui200)
-
-        quantidade_de_lances_recebido = len(self.leilao.lances)
-
-        self.assertEqual(1, quantidade_de_lances_recebido)
+        with self.assertRaises(ValueError):
+            self.leilao.propoe(self.lance_do_gui)
+            self.leilao.propoe(lance_do_gui200)
