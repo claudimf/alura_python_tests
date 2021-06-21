256fa7325e0aeab366b48166d39af55cc8dcb222
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-06-21 11:21:39 -0300

    Makes course class "Um pouco mais de testes"

--- tests/test_usuario.py
+++ tests/test_usuario.py
@@ -9,3 +9,32 @@ def test_deve_subtrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance(
     vini.propoe_lance(leilao, 50.0)
 
     assert vini.carteira == 50.0
+
+def test_deve_permitir_propor_lance_quando_o_valor_eh_menor_que_o_valor_da_carteira():
+    vini = Usuario('Vini', 100.0)
+
+    leilao = Leilao('Celular')
+
+    vini.propoe_lance(leilao, 1.0)
+
+    assert vini.carteira == 99.0
+
+
+def test_deve_permitir_propor_lance_quando_o_valor_eh_igual_ao_valor_da_carteira():
+    vini = Usuario('Vini', 100.0)
+
+    leilao = Leilao('Celular')
+
+    vini.propoe_lance(leilao, 100.0)
+
+    assert vini.carteira == 0.0
+
+def test_nao_deve_permitir_propor_lance_com_valor_maior_que_o_da_carteira():
+
+    vini = Usuario('Vini', 100.0)
+
+    leilao = Leilao('Celular')
+
+    vini.propoe_lance(leilao, 200.0)
+
+    assert vini.carteira == 100.0
